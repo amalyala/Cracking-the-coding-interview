ef7583c858fa72369623c71d78fd27fd881693f8
rahul sambari <sandslash.rahul@gmail.com>
2014-10-15 04:26:21 -0400

    Implementing Queue using two stacks

--- chap3_5.rb
+++ chap3_5.rb
@@ -7,8 +7,6 @@ class MyQueue
  attr_accessor :myQueue
  # In this constructor, we are creating two stacks and placing them inside hash for easy access.
  def initialize
-  @i = 1
-  @size = 3  # Defining size of each stack
   @myQueue = Hash.new # Creating a hash to hold two stacks.
   @myQueue[1] = Array.new # Creating first stack
   @myQueue[2] = Array.new # Creating second stack, which works in reverse order   
@@ -16,36 +14,46 @@ class MyQueue
  
  # This method is to insert elements into stack 1
  def enqueue(val)
-  if @myQueue[1].size <= @size
    @myQueue[1] << val 
-  elsif @myQueue[1].size > @size
-   puts "No Enought Space".red   
-  end
+   puts "Enqueued value #{val} into Stack 1".green
 end
  
+ # Always dequeued from Stack 2
  def dequeue
-  if @myQueue[2].empty? 
-   shift_first_to_second_stack 
+ # Checking if stack 2 is empty? then shift elements from stack 1 to 2.
+ # If stack 2 is not empty ? then directly pop elements from stack 2.
+  if @myQueue[2].empty?
+   shift_and_pop 
   elsif @myQueue[2].any? 
-   @myQueue[2].pop
+   popped =  @myQueue[2].pop
+   puts "Dequeue element #{popped} from Stack 2".yellow
   end
  end
  
- def shift_first_to_second_stack
-  @myQueue[1].size.times do 
-   @myQueue[2] << @myQueue[1].pop
+ # This method is called to shift all elements from stack 1 to stack 2.
+ def shift_and_pop
+  # Shifting every element of stack 1 into stack 2
+  if @myQueue[1].size > 0 then 
+   @myQueue[1].size.times do 
+    @myQueue[2] << @myQueue[1].pop
+   end
+  else
+   puts "No Elements to pop".red
+   return
   end
- dequeue
+  # After shifting, call dequeue again to pop the element.
+  dequeue  
  end
- 
 end
 
 @obj = MyQueue.new
+@obj.enqueue(1)
+@obj.enqueue(2)
 @obj.enqueue(3)
-@obj.enqueue(4)
-@obj.enqueue(5)
+@obj.dequeue
 @obj.dequeue
 
+puts "Final Values".cyan
 puts "-------------------"
-puts "#{@obj.myQueue}".cyan
+puts "#{@obj.myQueue}"
 puts "-------------------"
