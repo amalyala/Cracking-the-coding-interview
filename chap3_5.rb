df80439a1d6b7b7ea7b8f801a6e27880f9584251
rahul sambari <sandslash.rahul@gmail.com>
2014-10-15 03:58:26 -0400

    partial code

--- chap3_5.rb
+++ chap3_5.rb
@@ -1,26 +1,51 @@
 # This program is to create Queue like functionality using two stacks.
+# Very simple logic to implement
 require 'colorize'
 
 
 class MyQueue
  attr_accessor :myQueue
+ # In this constructor, we are creating two stacks and placing them inside hash for easy access.
  def initialize
-  @i = 2
-  @myQueue = Hash.new # Creating a hash to hold two stacks in place.
-  @myQueue[1] = Array.new(3) # Creating first stack
-  @myQueue[2] = Array.new(3) # Creating second stack, which works in reverse order   
+  @i = 1
+  @size = 3  # Defining size of each stack
+  @myQueue = Hash.new # Creating a hash to hold two stacks.
+  @myQueue[1] = Array.new # Creating first stack
+  @myQueue[2] = Array.new # Creating second stack, which works in reverse order   
  end
-
+ 
+ # This method is to insert elements into stack 1
+ def enqueue(val)
+  if @myQueue[1].size <= @size
+   @myQueue[1] << val 
+  elsif @myQueue[1].size > @size
+   puts "No Enought Space".red   
+  end
+end
+ 
+ def dequeue
+  if @myQueue[2].empty? 
+   shift_first_to_second_stack 
+  elsif @myQueue[2].any? 
+   @myQueue[2].pop
+  end
+ end
+ 
+ def shift_first_to_second_stack
+  @myQueue[1].size.times do 
+   @myQueue[2] << @myQueue[1].pop
+  end
+ dequeue
+ end
+ 
 end
 
 @obj = MyQueue.new
+@obj.enqueue(3)
+@obj.enqueue(4)
 @obj.enqueue(5)
-@obj.enqueue(6)
-@obj.enqueue(7)
-@obj.enqueue(8)
-@obj.enqueue(9)
-@obj.enqueue(10)
-@obj.enqueue(11)
+@obj.dequeue
+
 puts "-------------------"
 puts "#{@obj.myQueue}".cyan
 puts "-------------------"
