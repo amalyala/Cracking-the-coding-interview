e3d1667447bbb980269b971fa16d5aa36a276cd0
rahul sambari <sandslash.rahul@gmail.com>
2014-10-24 20:09:08 -0400

    Animal Shelter problem in chapter 3, question 7

--- chap3_7.rb
+++ chap3_7.rb
@@ -12,50 +12,55 @@ class AnimalShelter
  # This method is to push animals either "dog" or "cat" into the linked list.
  def enqueue val
   @shelter.push(val)
-  puts "Pushed value #{val} into Animal Shelter".green
-  p @shelter
+  puts "Pushed \"#{val}\" into Animal Shelter".green
  end
  
  # Method for dequeuing any animal with oldest arrival time.
  def dequeueAny
-  puts ""
   puts "Oldest animal is \"#{@shelter.shift}\"".yellow
   p @shelter
-  puts ""
  end
  
  # Method to get the oldest Dog in animal shelter
  def dequeueDog	
   if @shelter.first == "dog"
-   puts "Oldest Dog is removed #{@shelter.shift}".red
+   puts "Oldest #{@shelter.shift} is removed".cyan
    # Getting back any elements present inside buffer into shelter again.
    getBufferElements
+   p @shelter 
   elsif @shelter.first == "cat"
    @buffer.push(@shelter.shift) # Putting elements from @shelter to @buffer.
    dequeueDog # Using Recursion untill we get any dog.
+  else 
+   puts "No Animals in shelter"
+   return   
   end   
  end
 
  # Method to get the oldest Cat in animal shelter
  def dequeueCat
   if @shelter.first == "cat"
-   puts "Oldest Cat is removed #{@shelter.shift}".red
+   puts "Oldest #{@shelter.shift} is removed".cyan
    # Getting back any elements present inside buffer into shelter again.
    getBufferElements
-   p @shelter
+   p @shelter 
   elsif @shelter.first == "dog"
    @buffer.push(@shelter.shift) # Putting elements from @shelter to @buffer.
    dequeueCat # Using Recursion untill we get any dog.
+  else 
+   puts "No Animals in shelter".red
+   return  
   end   
  end
  
  # Getting back the elements into shelter if there are any elements in buffer.
  def getBufferElements
   if @buffer.any?
+  # Iterating the Buffer for elements
    @buffer.each do |f|
-    @shelter.unshift(f) 
+    @shelter.unshift(@buffer.pop) 
    end
-  end 
+  end
  end
   
 end
@@ -63,8 +68,10 @@ end
 @obj = AnimalShelter.new
 @obj.enqueue "dog"
 @obj.enqueue "cat"
-@obj.enqueue "cat"
-@obj.enqueue "dog"
 @obj.enqueue "dog"
 @obj.enqueue "cat"
 @obj.dequeueCat
+@obj.dequeueDog
+@obj.dequeueDog
+@obj.dequeueCat
+
